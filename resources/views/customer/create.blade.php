38117f7e8ba4fe26e6225d6058a72401d47d28de
LuisIrua23 <lairua@soy.sena.edu.co>
2024-07-16 00:46:50 -0500

    LUIS

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -7,33 +7,36 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="{{ route('customer.store') }}" method="POST" enctype="multipart/form-data" >
-        @csrf {{-- token o seguridad  --}}
-        <label  class="form-label">
-            Telefono:
+    <center>
+        <form action="{{ route('customer.store') }}" method="POST" enctype="multipart/form-data" >
+            @csrf {{-- token o seguridad  --}}
+            <label  class="form-label">
+                Telefono:
+                <br>
+                <input class="form-control" type="text" name="telefono"  class="form-control" required>
+            </label>
             <br>
-            <input class="form-control" type="text" name="telefono"  class="form-control" required>
-        </label>
-        <br>
-        <label  class="form-label">
-            Num_social:
+            <label  class="form-label">
+                Num_social:
+                <br>
+                <input class="form-control"  type="text" name="num_social" required>
+            </label>
             <br>
-            <input class="form-control"  type="text" name="num_social" required>
-        </label>
-        <br>
-        <label  class="form-label">
-            Codigo:
+            <label  class="form-label">
+                Codigo:
+                <br>
+                <input class="form-control"  type="text" name="codigo" required>
+            </label>
             <br>
-            <input class="form-control"  type="text" name="codigo" required>
-        </label>
-        <br>
-        <label  class="form-label">
-            Domicilio
-            <br>
-            <input class="form-control"  type="text" name="domicilio" required>
-        </label>
-        <br><br>
-        <button class="btn btn-primary" type="submit" >Enviar Formulario:</button>
-    </form>
+            <label  class="form-label">
+                Domicilio
+                <br>
+                <input class="form-control"  type="text" name="domicilio" required>
+            </label>
+            <br><br>
+            <button class="btn btn-primary" type="submit" >Enviar Formulario:</button>
+        </form>
+    </center>
+    
 </body>
 </html>
